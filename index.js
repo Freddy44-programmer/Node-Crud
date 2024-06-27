3cba0e57d9af760394b90c8802a61c439dc25bf3
freddy konyane <rangataft19@gmail.com>
2024-06-27 11:50:18 +0200

    add and view api created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require("mongoose")
+const Product = require('./models/product.model.js');
 const app = express()
 
 
@@ -14,10 +15,24 @@ app.listen(3000, () =>{
      res.send("Hello from Node API server")
   });
 
-  app.post('/api/products', (req, res) =>{
-   console.log(req.body);
-   res.send(req.body);
+  app.get('/api/products', async (req, res) =>{
+    try {
+       const products = await Product.find({});
+       res.status(200).json(products); 
+    } catch (error) {
+    res.status(500).json({ message: error.message});
+    }
   });
+
+  app.post('/api/products', async (req, res) =>{
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message});
+  }
+  });
+
   
 mongoose.connect("mongodb+srv://rangataft08:m6XE59moRy0FhLdT@backenddb.3xwhved.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB")
 .then(()=>{
