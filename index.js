a639af5d37d1025b1df00a06acf6e184f5a258a9
freddy konyane <rangataft19@gmail.com>
2024-06-27 12:10:15 +0200

    get product by id api created

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ app.listen(3000, () =>{
     }
   });
 
+  app.get('/api/products/:id', async (req, res) =>{
+    try {
+    const {id} = req.params;
+    const product= await Product.findById(id);
+    res.status(200).json(product);
+    } catch (error) {
+    res.status(500).json({ message: error.message});
+    }
+  });
+  
+
   app.post('/api/products', async (req, res) =>{
   try {
     const product = await Product.create(req.body);
