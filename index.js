747cd6389bbe4082615a40a7752af04bf502690f
freddy konyane <rangataft19@gmail.com>
2024-06-29 10:19:14 +0200

    delete and update api added

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ app.listen(3000, () =>{
      res.send("Hello from Node API server")
   });
 
+
+   // get all the product API
   app.get('/api/products', async (req, res) =>{
     try {
        const products = await Product.find({});
@@ -24,7 +26,8 @@ app.listen(3000, () =>{
     }
   });
 
-  app.get('/api/products/:id', async (req, res) =>{
+   // get a product using the Id API
+  app.get('/api/product/:id', async (req, res) =>{
     try {
     const {id} = req.params;
     const product= await Product.findById(id);
@@ -33,8 +36,8 @@ app.listen(3000, () =>{
     res.status(500).json({ message: error.message});
     }
   });
-  
 
+ // adding the products API
   app.post('/api/products', async (req, res) =>{
   try {
     const product = await Product.create(req.body);
@@ -43,6 +46,41 @@ app.listen(3000, () =>{
     res.status(500).json({ message: error.message});
   }
   });
+
+
+  // update a product based of the Id API
+  app.put('/api/product/:id', async (req, res) =>{
+    try {
+      const {id} = req.params;
+      const product= await Product.findByIdAndUpdate(id, req.body);
+      if (!product){
+      return res.status(404).json({message: "Product not found"});
+      }
+     const updatedProduct = await Product.findById(id);
+     res.status(200).json(updatedProduct);
+
+    } catch (error) {
+      res.status(500).json({ message: error.message});
+    }
+    });
+
+
+     // delete a product API
+
+     app.delete('/api/product/:id', async (req, res) =>{
+      try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+
+        if (!product){
+          return res.status(404).json({message: "Product not found"});
+          }
+          res.status(200).json({ message: "Product deleted successfully"});
+
+      } catch (error) {
+        res.status(500).json({ message: error.message});
+      }
+      });
 
   
 mongoose.connect("mongodb+srv://rangataft08:m6XE59moRy0FhLdT@backenddb.3xwhved.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB")
