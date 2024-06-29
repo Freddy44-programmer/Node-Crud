40a4f8b6cdef795636496ad3cded3159aec8e132
freddy konyane <rangataft19@gmail.com>
2024-06-29 12:08:24 +0200

    code restructured

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ app.use("/api/products", productRoute);
 mongoose.connect("mongodb+srv://rangataft08:m6XE59moRy0FhLdT@backenddb.3xwhved.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB")
 .then(()=>{
     console.log("Connected to database!");
+    app.listen(3000, () =>{
+      console.log("Server is running on port 3000")
+    })
 })
 .catch(() =>{
     console.log("Connection failed!");
